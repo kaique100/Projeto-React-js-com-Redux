ddb6a025ac70e177b5b39340ab8545f2e61b7311
Kaique <skaique833@gmail.com>
2021-02-05 21:02:38 -0200

    Finalizado o projeto de React com Redux

---
+++ front-end/src/reducers/index.js
@@ -0,0 +1,28 @@
+import { combineReducers } from "redux";
+
+const initialState = {
+    frutas: []
+}
+
+function reducer(state = initialState, action) {
+    if (action.type === "ADICIONAR") {
+        return { frutas: [ ...state.frutas, {...action.value}]}
+
+    }
+    else {
+        return state;
+    }
+}
+
+function tituloReducer(state= { titulo: "Feira" }, action) {
+    if(action.type === "ALTERAR"){
+        return { titulo: action.value };
+    }
+    else {
+        return state;
+    }
+}
+
+const reducers = combineReducers( {reducer, tituloReducer});
+
+export default reducers;
